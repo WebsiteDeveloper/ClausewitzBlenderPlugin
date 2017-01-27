7b37d74b45d364e2c234b5bf596b6b17222caf07
Bernhard Sirlinger <bernhard@sirlinger.de>
2017-01-27 21:06:00 +0100

    * Add project and remove old code

--- import-export-clausewitz/__init__.py
+++ import-export-clausewitz/__init__.py
@@ -51,15 +51,11 @@ def menu_func_import(self, context):
 
 def register():
     bpy.utils.register_module(__name__)
-    #bpy.utils.register_class(ClausewitzExporter)
-    #bpy.utils.register_class(ClausewitzImporter)
     bpy.types.INFO_MT_file_export.append(menu_func_export)
     bpy.types.INFO_MT_file_import.append(menu_func_import)
 
 
 def unregister():
     bpy.utils.unregister_module(__name__)
-    #bpy.utils.unregister_class(ClausewitzExporter)
-    #bpy.utils.unregister_class(ClausewitzImporter)
     bpy.types.INFO_MT_file_export.remove(menu_func_export)
     bpy.types.INFO_MT_file_import.remove(menu_func_import)
