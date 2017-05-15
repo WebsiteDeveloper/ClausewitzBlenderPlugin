a3ca0640ce5dcc430af1e60a5ce4abec6d621ffd
Oninoni <jan@cheap-purple.com>
2017-05-15 20:49:33 +0200

    Skeleton "finished"

--- import-export-clausewitz/importer.py
+++ import-export-clausewitz/importer.py
@@ -44,52 +44,33 @@ class PdxFileImporter:
                                 names[joint.index] = joint.name
 
                             bpy.ops.object.mode_set(mode='EDIT')
-
-                            #parentTransforms = {}
                             
                             for joint in shape.skeleton.joints:
                                 bone = amt.edit_bones.new(joint.name)
 
-                                #print("Loading Joint \"" + joint.name + "\\" + str(joint.index) + "\"")
-                                #print("Parent: \"" + names[joint.parent] + "\\" + str(joint.parent) + "\"")
-                                #print("Transformation Matrix: ")
-
-                                #print(str(joint.transform[0]) + "|" + str(joint.transform[1]) + "|" + str(joint.transform[2]) + "|" + str(joint.transform[3]))
-                                #print(str(joint.transform[4]) + "|" + str(joint.transform[5]) + "|" + str(joint.transform[6]) + "|" + str(joint.transform[7]))
-                                #print(str(joint.transform[8]) + "|" + str(joint.transform[9]) + "|" + str(joint.transform[10]) + "|" + str(joint.transform[11]))
-
-                                #transformationMatrix = mathutils.Matrix((joint.transform[0:3], joint.transform[4:7], joint.transform[8:11]))
                                 transformationMatrix = mathutils.Matrix()
                                 transformationMatrix[0][0:4] = joint.transform[0], joint.transform[3], joint.transform[6], joint.transform[9]
                                 transformationMatrix[1][0:4] = joint.transform[1], joint.transform[4], joint.transform[7], joint.transform[10]
                                 transformationMatrix[2][0:4] = joint.transform[2], joint.transform[5], joint.transform[8], joint.transform[11]
                                 transformationMatrix[3][0:4] = 0, 0, 0, 1
-                                #parentTransforms[joint.index] = transformationMatrix
+
                                 print(transformationMatrix.decompose())
 
                                 if joint.parent >= 0:
-                                    temp_transform = transformationMatrix #.inverted()
-                                    components = temp_transform.decompose()
 
                                     parent = amt.edit_bones[names[joint.parent]] 
                                     bone.parent = parent
                                     bone.use_connect = True 
-
-                                    mat_temp = components[1].to_matrix()
-                                    mat_temp.resize_4x4()
-
-                                    bone.tail = components[0] * mat_temp * mat_rot
-
-                                    #bone.tail = parent.tail + (mathutils.Vector((1, 1, 1)) * transformationMatrix * mat_rot)
-
-                                    #transformationMatrix *= parentTransforms[joint.parent]
                                 else:          
                                     bone.head = (0,0,0)
-                                    bone.tail = (0,0,0)
 
+                                temp_transform = transformationMatrix #.inverted()
+                                components = temp_transform.decompose()
 
-                                
+                                mat_temp = components[1].to_matrix()
+                                mat_temp.resize_4x4()
 
+                                bone.tail = -components[0] * mat_temp * mat_rot
                             bpy.ops.object.mode_set(mode='OBJECT')
 
                         print(str(len(shape.meshes)))
@@ -162,11 +143,10 @@ class PdxFileImporter:
                         print("ERROR ::: Invalid Object in World: " + str(shape))
             elif isinstance(node, pdx_data.PdxLocators):
                 for locator in node.locators:
-                    print("Locators")
-                    #obj = bpy.data.objects.new(locator.name, None)
-                    #bpy.context.scene.objects.link(obj)
-                    #obj.empty_draw_size = 2
-                    #obj.empty_draw_type = 'PLAIN_AXES'
-                    #obj.location = mathutils.Vector((locator.pos[0], locator.pos[1], locator.pos[2])) * mat_rot
+                    obj = bpy.data.objects.new(locator.name, None)
+                    bpy.context.scene.objects.link(obj)
+                    obj.empty_draw_size = 2
+                    obj.empty_draw_type = 'PLAIN_AXES'
+                    obj.location = mathutils.Vector((locator.pos[0], locator.pos[1], locator.pos[2])) * mat_rot
             else:
                 print("ERROR ::: Invalid node found: " + str(node))
